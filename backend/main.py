f9b706916e9c52f0cc18c759dfebbad084437ba5
Liang <chenliang.info@gmail.com>
2025-06-20 15:58:07 -0700

    done

--- backend/main.py
+++ backend/main.py
@@ -122,6 +122,6 @@ async def startup_event():
         raise
 
 if __name__ == "__main__":
-    print("Starting FastAPI server done...")
+    print("Starting FastAPI server done ...")
     import uvicorn
     uvicorn.run(app, host="127.0.0.1", port=8000) 
